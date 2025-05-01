475644fe0499fd5221a3cf5542b430796e4405cb
Sundial <rob@appold.com>
2025-05-01 14:26:46 +0000

    html..

--- conf.py
+++ conf.py
@@ -1,3 +1,5 @@
+import os
+
 project = 'Sundial'
 html_title = "Sundial Documentation"
 copyright = '2025'
@@ -6,16 +8,16 @@ author = 'Sundial Team'
 extensions = [
     'sphinx.ext.mathjax',
 ]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'furo'
+
 html_theme_options = {
-    'version_selector': False,
-    # Other theme options
+    "light_logo": "sundial_logo.png",
+    "dark_logo": "sundial_logo.png",
 }
+
 html_static_path = ['_static']
-html_css_files = [
-    'custom.css',
-]
-templates_path = ['_templates']
+html_css_files = ['custom.css']
